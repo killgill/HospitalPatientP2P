a7f1d5e613a44d12fecaafb8b29fab1a2ef30f90
killgill <gill@cooper.edu>
2017-11-12 17:34:59 -0800

    I have no idea what I'm doing

--- healthcenter.cpp
+++ healthcenter.cpp
@@ -1,79 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <unistd.h>
+#include <iostream>
+#include <fstream>
 
-#define MYPORT "30285" //port from 2 + last digits of student ID
-#define BACKLOG 0//pending connections the queue will hold - we only establish 1 connection, so we don't want any
-#define MAXDATASIZE 100 //max number of bytes we can get at once
+#define HCPORT "30285"    // the port users will be connecting to
 
-int main()
+#define MAXBUFLEN 1000
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-
-	struct addrinfo hints, *servinfo; //*servinfo points to results
-
-	memset(&hints, 0, sizeof hints); //ensures empty struct
-	hints.ai_family = AF_UNSPEC; //ipv4 or v6 is fine
-	hints.ai_socktype = SOCK_STREAM; //TCP stream sockeet
-	hints.ai_flags= AI_PASSIVE; //fill in IP
-
-	// checks for error with getaddrinfo and prints debug info
-	// also ensures that servinfo points to the addrinfos from the struct
-	int status;
-	if ((status = getaddrinfo(NULL,MYPORT,&hints,&servinfo)) != 0) {
-		fprintf(stderr, "gettaddrinfo error: %s\n", gai_strerror(status));
-		exit(1);
-	}
-
-
-
-	int sockfd = socket(PF_INET,SOCK_STREAM,0); //creates socket
-
-	//this block sorts out the address is already in use error
-	int yes = 1;
-	if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
-		perror("setsockopt");
-		exit(1);
-	}
-
-	bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen); //binds socket to port from getaddrinfo()
-
-	listen(sockfd, BACKLOG); //starts listening
-
-	// creates new socket for communication with client
-	struct sockaddr_storage clt_addr;
-	socklen_t addr_size = sizeof(clt_addr);
-	int new_socket= accept(sockfd, (struct sockaddr *)&clt_addr, &addr_size);
-
-	shutdown(sockfd, SHUT_RDWR);
-	close(sockfd); //since only one connection is required, the closes the listening socket after it has been established
-
-	char buf[MAXDATASIZE] = ""; //creates empty string for the buffer with the buffer length
-
-	int numbytes;
-
-	// Receives the packet, and also does a small error check.
-	if ((numbytes = recv(new_socket, buf, MAXDATASIZE-1, 0)) == -1)
-	{
-		perror("recv");
-		exit(1);
-	}
-
-	printf("%s\n", buf); //prints bugger
-
-	// generates and sends message to client
-	const char *msg = "OK!";
-	int msglen, bytes_sent;
-	msglen = strlen(msg);
-	bytes_sent = send(new_socket , msg, msglen, 0);
-
-	shutdown(new_socket, SHUT_RDWR);
-    close(new_socket);//closes socket to free up the port
-
-	exit(0);
-
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
+int main(void)
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, HCPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("Healthcenter ready to receive phase 1\n");
+
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    printf("Healthcenter: %s\n",
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s));
+    printf("listener: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("healthcenter: packet contains \"%s\"\n", buf);
+
+    close(sockfd);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	listen(sockhc, BACKLOG); //starts listening
+
+// 	// creates new socket for communication with client
+// 	struct sockaddr_storage clt_addr;
+// 	socklen_t addr_size = sizeof(clt_addr);
+// 	int new_socket= accept(sockhc, (struct sockaddr *)&clt_addr, &addr_size);
+
+// 	shutdown(sockhc, SHUT_RDWR);
+// 	close(sockhc); //since only one connection is required, the closes the listening socket after it has been established
+
+// 	char buf[MAXDATASIZE] = ""; //creates empty string for the buffer with the buffer length
+
+// 	int numbytes;
+
+// 	// Receives the packet, and also does a small error check.
+// 	if ((numbytes = recv(new_socket, buf, MAXDATASIZE-1, 0)) == -1)
+// 	{
+// 		perror("recv");
+// 		exit(1);
+// 	}
+
+// 	printf("%s\n", buf); //prints bugger
+
+// 	// generates and sends message to client
+// 	const char *msg = "OK!";
+// 	int msglen, bytes_sent;
+// 	msglen = strlen(msg);
+// 	bytes_sent = send(new_socket , msg, msglen, 0);
+
+// 	shutdown(new_socket, SHUT_RDWR);
+//     close(new_socket);//closes socket to free up the port
+
+// 	exit(0);
+
+//}
